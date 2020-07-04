6ac2c592d3d8ac2b82c7dd104ef201b016a490ec
15891075984 <920702811@qq.com>
2020-07-04 23:48:27 +0800

    port

--- serve/app.js
+++ serve/app.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-07-04 18:50:13
- * @LastEditTime: 2020-07-04 18:56:36
+ * @LastEditTime: 2020-07-04 23:48:16
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue-demo\serve\app.js
@@ -11,4 +11,4 @@ var path = require('path')
 var app = express()
  
 app.use(express.static(path.join(__dirname, '../dist')))
-app.listen(8088);
+app.listen(8080);
